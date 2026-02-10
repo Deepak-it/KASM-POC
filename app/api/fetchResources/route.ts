8870afeb05a926b9014d4796c21116e26cd35c7f
Deepak <deepak@Mac.lan>
2026-02-10 23:56:20 +0530

    filter region backend

--- app/api/fetchResources/route.ts
+++ app/api/fetchResources/route.ts
@@ -3,9 +3,10 @@ import { EC2Client, DescribeInstancesCommand } from "@aws-sdk/client-ec2";
 import { getServerSession } from "next-auth";
 import { authOptions } from "../../lib/auth";
 
-export async function GET() {
+export async function GET(req: Request) {
   const session: any = await getServerSession(authOptions as any);
-
+  const url = new URL(req.url)
+  const region = (url.searchParams.get('region') || process.env.AWS_REGION_ENV)
   if (!session?.user?.email) {
     return NextResponse.json(
       { success: false, message: "Unauthorized" },
@@ -16,7 +17,7 @@ export async function GET() {
   const email = session.user.email;
 
   const client = new EC2Client({
-    region: process.env.AWS_REGION_ENV,
+    region: region,
   });
 
   const command = new DescribeInstancesCommand({
